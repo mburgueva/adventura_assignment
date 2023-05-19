c19e210d3bbfd9ee6dfb1ac7294e5c3196fa9789
Мадина Бургуева <madinaburgueva@MacBook-Air-Madina.local>
2023-05-19 17:02:28 +0200

    Test Comment

--- src/cz/vse/adventura/logika/Prostor.java
+++ src/cz/vse/adventura/logika/Prostor.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-
+// Room with Things
 /**
  * Trida Prostor - popisuje jednotlivé prostory (místnosti) hry
  *
